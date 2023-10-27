e631b75f56c4e7912aecaa4c03f7acf93d036f9a
Wesley Marques <wesleymarquesdc@gmail.com>
2023-10-27 19:56:40 -0300

    update with sqrt function

--- esptriangular.c
+++ esptriangular.c
@@ -1,22 +1,12 @@
 #include <stdio.h>
 #include <math.h>
 
-// retorna a raiz quadrada, caso n seja um quadrado perfeito,
-// ou a raiz quadrada do menor quadrado perfeito maior que n
-int whole_square_root(int n){
-  int i = 0;
-  while(i*i < n) i++;
-  return i;
-}
-
 int main(){
-  int point = 0;
-  int x = 0;
-  int y = 0;
+  int point, x, y;
 
   scanf("%d", &point);
 
-  int root = whole_square_root(point);
+  int root = (int)sqrt((double)point);
 
   if(root*root == point){ //quadrado perfeito
     if(root%2 == 0){ //quadrado perfeito par
@@ -27,9 +17,8 @@ int main(){
       y = -root/2;
     }
   }else{ //não é quadrado perfeito
-    root--; //raiz quadrada do maior quadrado perfeito menor que n
     int dig = root*root; //maior quadrado perfeito menor que n
-    int aux = point - dig; //o quanto point é maior que dig
+    int aux = point - dig; //quanto point é maior que dig
 
     if(dig%2 == 0){ //dig é par
       x = -root + aux;
